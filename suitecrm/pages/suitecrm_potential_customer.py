c6a635ebba4dd6c13255be940f4795f7f9bdb70e
roxana.tejada <rtejadasilva@gmail.com>
2020-09-05 20:21:18 +0200

    Correcciones Test: contact_customer, budgets. Creacion nueva Test: potential_customer

--- suitecrm/pages/suitecrm_potential_customer.py
+++ suitecrm/pages/suitecrm_potential_customer.py
@@ -9,18 +9,32 @@ class PotentialCustomer(SuitecrmBasePage):
     BUTTON_CREATE = (By.LINK_TEXT, 'Crear')
     CREATE_POTENTIAL_CUSTOMER = (By.LINK_TEXT, 'Crear Clientes Potenciales')
 
-    ACCOUNTS = (By.LINK_TEXT, 'Ver Cuentas')
+    POTENTIAL_CLIENTS = (By.LINK_TEXT, 'Ver Clientes Potenciales')
     FILTER = (By.XPATH, "//a[@title = 'Filtro']")
     RAPID_FILTER = (By.LINK_TEXT, 'Filtro rápido')
     WINDOW_VISIBLE = (By.ID, 'searchDialog')
-    BUTTON_NAME = (By.ID, 'name_basic')
+    BUTTON_NAME = (By.ID, 'search_name_basic')
     SEARCH = (By.ID, 'search_form_submit')
 
     def page_account(self):
 
         self.menu_select_option(self.BUTTON_CREATE, self.CREATE_POTENTIAL_CUSTOMER)
 
-    def create_potential_customer(self):
+    def create(self):
 
         potential_customer = CreateNew(self.driver)
-        potential_customer.create()
+        name = potential_customer.customer()
+        return name
+
+    def search(self, potential_customer):
+
+        self.wait_selector_visible(self.POTENTIAL_CLIENTS)
+
+        search_opportunity = SearchUsers(self.driver)
+        search_opportunity.access_option = self.POTENTIAL_CLIENTS
+        search_opportunity.press_filter = self.FILTER
+        search_opportunity.press_rapid_filter = self.RAPID_FILTER
+        search_opportunity.visible_selector = self.WINDOW_VISIBLE
+        search_opportunity.contact_name = self.BUTTON_NAME
+        value = search_opportunity.search_user(potential_customer)
+        return value
